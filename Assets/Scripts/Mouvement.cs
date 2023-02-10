6e7ee43546224529434a85ac95cd640ee074f29b
Firas Sediri <sfiras1111@gmail.com>
2023-02-10 11:25:19 +0100

    Update Mouvement.cs

--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -4,18 +4,30 @@ public class Mouvement : MonoBehaviour
 {
     public GameManager gameManager;
     public Rigidbody2D rigid2D { get; private set; }
+    // Variable to store the direction of movement
     Vector2 direction = Vector2.down;
+
+    // Variable to store the direction of movement
     public KeyCode inputUp = KeyCode.Z;
     public KeyCode inputDown = KeyCode.S;
     public KeyCode inputRight = KeyCode.D;
     public KeyCode inputLeft = KeyCode.Q;
+
+    // Speed of movement
     public float speed = 4.0f;
+
+    // Animated sprite renderers for different directions
     public AnimatedSpriteRenderer spriteRendererUp;
     public AnimatedSpriteRenderer spriteRendererDown;
     public AnimatedSpriteRenderer spriteRendererRight;
     public AnimatedSpriteRenderer spriteRendererLeft;
+
+    // Animated sprite renderers for the death spriteRenderer
     public AnimatedSpriteRenderer spriteRendererDeath;
+
+    // Variable to store the currently active sprite renderer
     private AnimatedSpriteRenderer activeSpriteRenderer;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -26,6 +38,7 @@ public class Mouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if the input for movement is being pressed
         if (Input.GetKey(inputUp))
         {
             SetDirection(Vector2.up,spriteRendererUp);
@@ -47,6 +60,7 @@ public class Mouvement : MonoBehaviour
             SetDirection(Vector2.zero,activeSpriteRenderer);
         }
     }
+    // Method to set the direction and the sprite renderer to the current direction
     private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
     {
         direction = newDirection;
@@ -61,15 +75,18 @@ public class Mouvement : MonoBehaviour
     {
         Vector2 position = rigid2D.position;
         Vector2 translation = direction * speed * Time.fixedDeltaTime;
+        // Moving the rigid body component to the new position
         rigid2D.MovePosition(position + translation);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Explosion"))
         {
+            // Start the death sequence
             DeathSequence();
         }
     }
+    // DeathSequence is called when the player collides with an explosion
     public void DeathSequence()
     {
         enabled = false;
@@ -81,6 +98,7 @@ public class Mouvement : MonoBehaviour
         spriteRendererDeath.enabled = true;
         Invoke(nameof(DeathSequenceEnded), 1.25f);
     }
+    // Once the death sequence has ended the gameManager checks for the winner of the game
     public void DeathSequenceEnded()
     {
         gameObject.SetActive(false);
